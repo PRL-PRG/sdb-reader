fd569e7454caccc834b2521e4ff4ff62f5dd6022
Filip Krikava <krikava@gmail.com>
2018-08-18 17:42:26 +0200

    Removed the old files

--- scala/src/main/scala/cz/cvut/fit/prl/scalaImplicit/processors/Processor.scala
+++
@@ -1,8 +0,0 @@
-package cz.cvut.fit.prl.scalaImplicit.processors
-
-import cz.cvut.fit.prl.scalaImplicit.SymbolRepository
-
-import scala.meta.internal.semanticdb3.TextDocument
-
-trait Processor
-    extends ((Traversable[TextDocument], SymbolRepository) => Result) {}
